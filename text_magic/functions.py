df2ca837fcb20ffbbe9891a48ebbaf438d0db157
Hugo1336 <julian.hagenbucher@gmx.at>
2025-11-18 11:33:09 +0100

    created libray with text funtions

---
+++ text_magic/functions.py
@@ -0,0 +1,30 @@
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from string import punctuation
+
+PUNCTUATIONS = punctuation + "“”‘’—–"
+
+
+
+def clean_to_lower(text:str) -> str:
+    
+    return text.lower()
+
+def clean_punctuation(text:str) -> str:
+    output_string = ""
+    for char in text:
+        if char not in PUNCTUATIONS:
+            output_string += char
+    return output_string
+
+def clean_strip(text:str) -> str:
+    for _ in range(10):
+        text = text.replace("  ", " ")
+    return text.strip()
+
+def pipeline(text:str) -> str:
+    text = clean_to_lower(text)
+    text = clean_punctuation(text)
+    text = clean_strip(text)
+    return text
